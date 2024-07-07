347893500c3b2df8ebd45442c161708d38b039e8
NobilisPerfectum <148438143+NobilisPerfectum@users.noreply.github.com>
2024-07-07 22:42:46 +0530

    Delete src directory [skip ci]

--- src/app/product/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-import Link from 'next/link'
-export default function DashboardPage() {
-    return (
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <Link href="/product/1">Goto Product category</Link>
-        This is a dashboard page
-      </main>
-    );
-  }
