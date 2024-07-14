b897912d2f6aa8f2b6e7604eb621b95e3673c946
NobilisPerfectum <148438143+NobilisPerfectum@users.noreply.github.com>
2024-07-14 20:32:38 +0530

    Add files via upload

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,15 +6,28 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: 'class', // Enable dark mode using the 'class' strategy
   theme: {
     extend: {
+      // Define custom properties for dark mode under the 'extend' key
+      colors: {
+        // Example: Define a dark background and text colors for dark mode
+        background: {
+          DEFAULT: '#ffffff', // Light mode background
+          dark: '#1a202c' // Dark mode background
+        },
+        text: {
+          DEFAULT: '#1a202c', // Light mode text color
+          dark: '#ffffff' // Dark mode text color
+        }
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
 };
+
 export default config;
