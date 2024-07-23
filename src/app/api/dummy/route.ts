a775efb5b832768ed09576b85264e31ac23342eb
NobilisPerfectum <148438143+NobilisPerfectum@users.noreply.github.com>
2024-07-23 20:46:50 +0530

    Update route.ts

--- src/app/api/dummy/route.ts
+++ src/app/api/dummy/route.ts
@@ -1,29 +1,10 @@
 import { NextRequest, NextResponse } from 'next/server';
-
-type Product = {
-  id: number;
-  name: string;
-  description: string;
-  price: number;
-};
-
-type Data = {
-  message: string;
-  data: {
-    products: Product[];
-  };
-};
-
-const products: Product[] = [
-  { id: 1, name: 'Product 1', description: 'Description 1', price: 99.99 },
-  { id: 2, name: 'Product 2', description: 'Description 2', price: 199.99 },
-  // Add more products as needed
-];
+import item, {Item} from './product'
 
 export const GET = function(req: NextRequest) {
   return NextResponse.json({
     message: 'Success',
-    data: { products },
+    data: { item },
   });
 };
 
