c81732ca5adb991ad8f5e6d4d71c633c20f50d1d
NobilisPerfectum <148438143+NobilisPerfectum@users.noreply.github.com>
2024-07-23 20:53:11 +0530

    Create index.tsx

---
+++ src/components/organisms/Footer/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <footer style={{ backgroundColor: 'var(--secondary-color)', padding: '1rem', color: 'white', textAlign: 'center' }}>
+      <p>© 2024 My Application. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
